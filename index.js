871717cc3bc820521d87dcff1b5404fb4f88a7d5
Sam Hazinski <shazinski@gmail.com>
2025-02-18 20:21:52 -0500

    App framework

---
+++ index.js
@@ -0,0 +1,27 @@
+//Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//Create an array of questions for user input
+const questions = [];
+
+// a function to write priority list file
+function generatePriorityList() {
+
+}
+
+// Create a function to initialize app
+function init() {
+inquirer
+.prompt([
+    
+])
+.then((answers) =>{
+    
+})
+
+
+}
+
+// Function call to initialize app
+init();
